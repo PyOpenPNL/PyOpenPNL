945fe74b60e827fc698ce3ce2113346942036d09
Tim O'Shea <tim.oshea753@gmail.com>
2016-01-21 19:37:21 -0500

    print out learned factors ...

--- qa2_dbn_complex.py
+++ qa2_dbn_complex.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import openpnl
 import numpy as np
+np.random.seed(1337)
 # Still broken ...
 
 print " *** SET UP MODEL **"
@@ -26,9 +27,18 @@ pLearn = openpnl.CEMLearningEngineDBN.Create( pDBN );
 pLearn.SetData(evidencesOut)
 pLearn.Learn()
 
+print " *** model before learning ... "
 for i in range(0,4):
     pCPD1 = pArHMM.GetFactor(i)
     pMat = pCPD1.GetMatrix(openpnl.matTable)
     cpd = openpnl.convertVector(pMat.ConvertToDense().GetVector())
     print cpd
     
+print " *** model after learning ... "
+for i in range(0,4):
+    pCPD2 = pDBN.GetFactor(i)
+    pMat = pCPD2.GetMatrix(openpnl.matTable)
+    cpd = openpnl.convertVector(pMat.ConvertToDense().GetVector())
+    print cpd
+
+
