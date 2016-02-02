d8a22e0368cbf971c11c855c63eb3ed2bc1f7fb7
Tim O'Shea <tim.oshea753@gmail.com>
2016-02-02 13:24:37 -0500

    adding python distutils build

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup, Extension
+
+swig_inc = [
+    "/usr/local/include/",
+    "/usr/local/include/opencx/",
+    "/usr/include/",
+    "/usr/include/opencx/",
+    ]
+swig_inc_flags = map(lambda x: "-I%s"%(x), swig_inc)
+
+openpnl_module = Extension('openpnl',
+                sources=['src/openpnl.i'],
+                swig_opts=['-builtin', '-c++'] + swig_inc_flags,
+                include_dirs=swig_inc,
+                runtime_library_dirs=[],
+                libraries=["src/openpnl"],
+                extra_compile_args=['-Wno-write-strings','-Wno-maybe-uninitialized'])
+
+setup(  name            = 'openpnl',
+        version         = '0.1',
+        author          = 'PyOpenPNL',
+        author_email    = 'contact@inferred.info',
+        description     = '',
+        license         = 'GPLv3',
+        url             = '',
+        platforms       = ['x86_64'],
+        ext_modules     = [openpnl_module],
+        py_modules      = ["src/openpnl"],
+        packages        = ['openpnl'],
+        package_dir     = {'openpnl': 'src'},
+        package_data    = {'openpnl': ['data/*.dat']} )
